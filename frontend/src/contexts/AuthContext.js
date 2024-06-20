236a730595eff0d91a476f01bc980e7350ac6dae
Utkarsh Raj <uraj33175@gmail.com>
2024-06-20 10:04:16 +0530

    new

---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,75 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+import auth from "../config/firebase";
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  function register(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        return updateProfile(user, {
+          displayName: email.split('@')[0], // Example: Set displayName to the part of email before '@'
+        });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Firebase registration error:", errorCode, errorMessage);
+        setError(errorMessage); // Set error state for displaying to user
+        throw error; // Re-throw error to propagate it further if needed
+      });
+  }
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logout() {
+    return signOut(auth);
+  }
+
+  function updateUserProfile(user, profile) {
+    return updateProfile(user, profile);
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    error,
+    setError,
+    login,
+    register,
+    logout,
+    updateUserProfile,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
